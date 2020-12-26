5f802a69bd934abada790650bbc57973ab394cfd
rookie.w <wanrage@naver.com>
2020-12-26 18:20:21 +0900

    💄 UIColor extension 추가

--- Dear-World/Dear-World/Source/Common/UIColor.swift
+++ Dear-World/Dear-World/Source/Common/UIColor.swift
@@ -5,4 +5,14 @@
 //  Created by rookie.w on 2020/12/26.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    public static let breathingWhite: UIColor = UIColor(named: "breathingWhite")!
+    public static let grayWhite: UIColor = UIColor(named: "grayWhite")!
+    public static let illuminatingYellow: UIColor = UIColor(named: "illuminatingYellow")!
+    public static let livelyBlue: UIColor = UIColor(named: "livelyBlue")!
+    public static let nightBlue: UIColor = UIColor(named: "nightBlue")!
+    public static let refreshingWhite: UIColor = UIColor(named: "refreshingWhite")!
+    public static let warmBlue: UIColor = UIColor(named: "warmBlue")!
+}
