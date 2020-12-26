feee9eb68cbe796a9e8184fbfbe941f1dc4a3828
rookie.w <wanrage@naver.com>
2020-12-27 03:01:58 +0900

    🔨 DiscoverReactor & 테스트를위한 APIMock, MessageMock 생성

---
+++ Dear-World/Dear-World/Source/Presentation/Scene/Discover/DiscoverReactor.swift
@@ -0,0 +1,46 @@
+//
+//  DiscoverReactor.swift
+//  Dear-World
+//
+//  Created by rookie.w on 2020/12/27.
+//
+
+import Foundation
+import ReactorKit
+
+class DiscoverReactor: Reactor {
+    enum Action {
+        case countryDidChanged
+    }
+    enum Mutation {
+        case setMessages(result: [MessageMock])
+    }
+    struct State {
+        var messageCount: Int = 0
+        //TODO: country는 String이 아니라 다른 값으로 저장
+        var country: String = "Whole World"
+        var messages: [MessageMock] = []
+    }
+    var initialState: State
+
+    init() {
+        self.initialState = State()
+    }
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .countryDidChanged :
+            return APIMock().getMessages(page: 1)
+                .map{Mutation.setMessages(result: $0)}
+        }
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState: State = state
+        switch mutation {
+        case let .setMessages(results) :
+            newState.messages = results
+        }
+        return newState
+    }
+}
---
+++ Dear-World/Dear-World/Source/Presentation/Scene/Discover/MessageMock.swift
@@ -0,0 +1,16 @@
+//
+//  MessageMock.swift
+//  Dear-World
+//
+//  Created by rookie.w on 2020/12/27.
+//
+
+import Foundation
+public struct MessageMock {
+    var emoji = "🎅🏻"
+    var name = "Judy"
+    var country: String = "🇰🇷"
+    var countryName: String = "South Korea"
+    var detail: String = "Hello world, dont worry because you’re strong enough to overcome this corona blue. e end of the day, trust yourself. more"
+    var likes: Int = 10
+}
