c997d8ca1e1ff30960b5f02224a1a12134915060
rookie.w <wanrage@naver.com>
2020-12-26 21:34:10 +0900

    💄 MessageTableViewCell 생성

---
+++ Dear-World/Dear-World/Source/Presentation/Scene/Discover/Cell/MessageTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  MessageTableViewCell.swift
+//  Dear-World
+//
+//  Created by rookie.w on 2020/12/26.
+//
+
+import UIKit
+
+class MessageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
