a65a1824c3157c627c477538baae1ee3e78b6cf0
붱이 <o.o.wl.soft@gmail.com>
2020-12-26 14:52:24 +0900

    ✨ 기본 ViewController 구현 (#3)

---
+++ Dear-World/Dear-World/Source/Presentation/Scene/Send Message/SendMessageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SendMessageViewController.swift
+//  Dear-World
+//
+//  Created by dongyoung.lee on 2020/12/26.
+//
+
+import UIKit
+
+class SendMessageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
