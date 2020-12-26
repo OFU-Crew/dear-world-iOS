81afa7561499902c01050e226c79593370e35c0e
이동영 <o.o.wl.soft@gmail.com>
2020-12-26 11:37:10 +0900

    :art: Swift Lint 적용 & 코드 수정

--- Dear-World/Dear-WorldTests/Dear_WorldTests.swift
+++ Dear-World/Dear-WorldTests/Dear_WorldTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import Dear_World
 
-class Dear_WorldTests: XCTestCase {
+class DearWorldTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
