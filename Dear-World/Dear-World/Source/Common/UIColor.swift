a02766f5bc47acc4610fde48a6e98fd478d19b2e
Yongwan Kim <33171230+wan088@users.noreply.github.com>
2020-12-26 18:25:40 +0900

    💄 UIColor 익스텐션 파일 생성 (#5)
    * 💄 UIColor 익스텐션 파일 생성
    
    * 💄 UIColor extension 추가
    
    * 💄 loveRed 컬러 추가

---
+++ Dear-World/Dear-World/Source/Common/UIColor.swift
@@ -0,0 +1,19 @@
+//
+//  UIColor.swift
+//  Dear-World
+//
+//  Created by rookie.w on 2020/12/26.
+//
+
+import UIKit
+
+extension UIColor {
+    public static let breathingWhite: UIColor = UIColor(named: "breathingWhite")!
+    public static let grayWhite: UIColor = UIColor(named: "grayWhite")!
+    public static let illuminatingYellow: UIColor = UIColor(named: "illuminatingYellow")!
+    public static let livelyBlue: UIColor = UIColor(named: "livelyBlue")!
+    public static let nightBlue: UIColor = UIColor(named: "nightBlue")!
+    public static let refreshingWhite: UIColor = UIColor(named: "refreshingWhite")!
+    public static let warmBlue: UIColor = UIColor(named: "warmBlue")!
+    public static let loveRed: UIColor = UIColor(named: "loveRed")!
+}
