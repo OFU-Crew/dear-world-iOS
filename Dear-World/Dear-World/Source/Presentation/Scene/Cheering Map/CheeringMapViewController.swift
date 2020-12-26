3b44202ae55256f35bace32c2bbc61d427a7a25c
이동영 <o.o.wl.soft@gmail.com>
2020-12-26 14:43:46 +0900

    ✨ 기본 ViewController 구현

--- Dear-World/Dear-World/Source/Presentation/Scene/Cheering Map/CheeringMapViewController.swift
+++ Dear-World/Dear-World/Source/Presentation/Scene/Cheering Map/CheeringMapViewController.swift
@@ -13,9 +13,20 @@ final class CheeringMapViewController: UIViewController {
     init() {
         super.init(nibName: nil, bundle: nil)
         self.view.backgroundColor = .green
+        
+        setupAttribute()
+        setupLayout()
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
+    }
+    
+    private func setupAttribute() {
+        
+    }
+    
+    private func setupLayout() {
+        
     }
 }
