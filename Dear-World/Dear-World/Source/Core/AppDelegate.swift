2576b9e1195fae4c1baf472703f429a6d3c17350
Yongwan Kim <33171230+wan088@users.noreply.github.com>
2020-12-31 04:46:43 +0900

    Feature/custom tabber (#13)
    * 🍱 tabBar 관련 asset 추가
    
    * 🔨 tabBar 구현

--- Dear-World/Dear-World/Source/Core/AppDelegate.swift
+++ Dear-World/Dear-World/Source/Core/AppDelegate.swift
@@ -4,8 +4,10 @@
 //
 //  Created by dongyoung.lee on 2020/12/24.
 //
-
+import SnapKit
+import Then
 import UIKit
+import RxSwift
 
 @main
 final class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,18 +17,56 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-//    let tabBarController: UITabBarController = UITabBarController()
-//    tabBarController.addChild(DiscoverViewController())
-//    tabBarController.addChild(CheeringMapViewController())
-//    
-    
+  
     let mainWindow: UIWindow = UIWindow()
     window = mainWindow
-    let viewController = CheeringMapViewController()
-    let reactor = CheeringMapReactor()
-    viewController.reactor = reactor
-    mainWindow.rootViewController = viewController
+    mainWindow.rootViewController = makeTabBarController()
     mainWindow.makeKeyAndVisible()
+    return true
+  }
+  func makeTabBarController() -> UITabBarController {
+    return UITabBarController().then {
+      $0.addChild(DiscoverViewController().then {
+        $0.tabBarItem.image = UIImage(named: "message_default")?
+          .withRenderingMode(.alwaysOriginal)
+        $0.tabBarItem.selectedImage = UIImage(named: "message")?
+          .withRenderingMode(.alwaysOriginal)
+      })
+      $0.addChild(SendMessageViewController().then {
+        $0.tabBarItem.image = UIImage(named: "add")?
+          .withRenderingMode(.alwaysOriginal)
+      })
+      $0.addChild(CheeringMapViewController().then {
+        $0.tabBarItem.image = UIImage(named: "earth_default")?
+          .withRenderingMode(.alwaysOriginal)
+        $0.tabBarItem.selectedImage = UIImage(named: "earth_icon")?
+          .withRenderingMode(.alwaysOriginal)
+      })
+      $0.tabBar.barTintColor = .white
+      $0.tabBar.layer.masksToBounds = true
+      $0.tabBar.layer.cornerRadius = 10
+      $0.tabBar.layer.borderWidth = 0.02
+      //TODO: 그림자 넣기
+      
+      $0.delegate = self
+    }
+  }
+}
+extension AppDelegate: UITabBarControllerDelegate {
+  func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+    if let _ = viewController as? SendMessageViewController {
+      let vc = SendMessageViewController()
+      tabBarController.addChild(vc)
+      tabBarController.view.addSubview(vc.view)
+      vc.view.frame.origin.y = tabBarController.view.frame.height
+      UIView.animate(withDuration: 0.2) {
+        vc.view.frame.origin.y = 0
+      } completion: { (_) in
+        tabBarController.willMove(toParent: tabBarController)
+      }
+
+      return false
+    }
     return true
   }
 }
