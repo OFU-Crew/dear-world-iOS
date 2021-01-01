0134517b2006ae4ed77afa8bfae476f0c7a655fa
이동영 <o.o.wl.soft@gmail.com>
2021-01-01 18:36:13 +0900

    :sparkles: 커스텀 탭바 구현 수정

---
+++ Dear-World/Dear-World/Source/Presentation/Scene/Main/MainTabBarController.swift
@@ -0,0 +1,61 @@
+//
+//  MainTabBarViewController.swift
+//  Dear-World
+//
+//  Created by dongyoung.lee on 2020/12/31.
+//
+
+import UIKit
+
+final class MainTabBarController: UITabBarController {
+  private let centerButton: UIButton = UIButton()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    setupTabBar()
+    self.delegate = self
+  }
+  
+  private func setupTabBar() {
+    let discoverViewController: UIViewController = DiscoverViewController().then {
+      $0.tabBarItem.image = UIImage(named: "message_default")?
+        .withRenderingMode(.alwaysOriginal)
+      $0.tabBarItem.selectedImage = UIImage(named: "message")?
+        .withRenderingMode(.alwaysOriginal)
+      $0.reactor = DiscoverReactor()
+    }
+    let sendMessageViewController: UIViewController = SendMessageViewController().then {
+      $0.reactor = SendMessageReactor()
+      $0.tabBarItem.image = UIImage(named: "add")?
+        .withRenderingMode(.alwaysOriginal)
+    }
+    let cheeringMapViewController: UIViewController = CheeringMapViewController().then {
+      $0.reactor = CheeringMapReactor()
+      $0.tabBarItem.image = UIImage(named: "earth_default")?
+        .withRenderingMode(.alwaysOriginal)
+      $0.tabBarItem.selectedImage = UIImage(named: "earth_icon")?
+        .withRenderingMode(.alwaysOriginal)
+    }
+    self.addChild(discoverViewController)
+    self.addChild(sendMessageViewController)
+    self.addChild(cheeringMapViewController)
+  }
+  
+  private func presentSendMessageViewController() {
+    let sendMessageViewController: UIViewController = SendMessageViewController().then {
+      $0.reactor = SendMessageReactor()
+      $0.modalPresentationStyle = .fullScreen
+    }
+    self.present(sendMessageViewController, animated: true, completion: nil)
+  }
+}
+extension MainTabBarController: UITabBarControllerDelegate {
+  func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+    if viewController.isKind(of: SendMessageViewController.self) {
+      presentSendMessageViewController()
+      return false
+    }
+    return true
+  }
+}
---
+++ Dear-World/Dear-World/Source/Presentation/Scene/Main/MenuTabBar.swift
@@ -0,0 +1,12 @@
+//
+//  MenuTabBar.swift
+//  Dear-World
+//
+//  Created by dongyoung.lee on 2020/12/31.
+//
+
+import UIKit
+
+final class MainTabBar: UITabBar {
+  var centerButton: UIButton?
+}
