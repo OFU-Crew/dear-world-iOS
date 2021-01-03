fef3f127d3a32d0271b89e694523649e57898b44
이동영 <o.o.wl.soft@gmail.com>
2021-01-04 01:33:02 +0900

    :bento: Color Asset 반입

--- Dear-World/Dear-World/Source/Common/Extension/UIColor+.swift
+++ Dear-World/Dear-World/Source/Common/Extension/UIColor+.swift
@@ -16,4 +16,10 @@ extension UIColor {
   public static let refreshingWhite: UIColor = UIColor(named: "refreshingWhite")!
   public static let warmBlue: UIColor = UIColor(named: "warmBlue")!
   public static let loveRed: UIColor = UIColor(named: "loveRed")!
+  public static let mapLevel0: UIColor = UIColor(named: "mapLevel0")!
+  public static let mapLevel1: UIColor = UIColor(named: "mapLevel1")!
+  public static let mapLevel2: UIColor = UIColor(named: "mapLevel2")!
+  public static let mapLevel3: UIColor = UIColor(named: "mapLevel3")!
+  public static let mapLevel4: UIColor = UIColor(named: "mapLevel4")!
+  public static let mapLevel5: UIColor = UIColor(named: "mapLevel5")!
 }
